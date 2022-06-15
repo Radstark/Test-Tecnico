c55310220503eaf706330bfa7d49bce683156dcb
Radstark <tommaso.merlin.97@gmail.com>
2022-06-15 21:01:08 +0200

    Parameterized keys in config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# region ingest
+KEY = "key"
+PAYLOAD = "payload"
+# endregion
+
+# region retrieve
+DATE_FROM = "Date_from"
+DATE_TO = "Date_to"
+# endregion
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 from typing import Dict, Union
 
+import config as cf
+
 
 def ingest(request: Dict[str, Union[int, str]]):
-    key = request["key"]
-    payload = request["payload"]
+    key = request[cf.KEY]
+    payload = request[cf.PAYLOAD]
 
     if random.randrange(1, 11) == 1:
         response_code = 500
@@ -13,12 +15,12 @@ def ingest(request: Dict[str, Union[int, str]]):
 
 
 def retrieve(request: Dict[str, str]):
-    date_from = request["Date_from"]
-    date_to = request["Date_to"]
+    date_from = request[cf.DATE_FROM]
+    date_to = request[cf.DATE_TO]
 
 
 if __name__ == "__main__":
     ingest({
-        "key": random.randrange(1, 7),
-        "payload": "".join(["a" for _ in range(random.randrange(10, 256))])
+        cf.KEY: random.randrange(1, 7),
+        cf.PAYLOAD: "".join(["a" for _ in range(random.randrange(10, 256))])
     })
